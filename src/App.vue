566c1122c6f54e3beefb8dc6c79f4a0d89bb0255
Dewa Putra Hernanda <dewananda089@gmail.com>
2024-08-28 23:09:14 +0700

    update form modal

--- src/App.vue
+++ src/App.vue
@@ -24,14 +24,22 @@
       <div class="flex justify-between items-center pb-5 basis-1/5">
         <h1 class="text-2xl">Add Memo</h1>
         <button class="text-xl w-fit h-fit"
-          @click="toggleModal=false"
+          @click="closeModal"
         >X</button>
       </div>
 
       <form class="w-full flex flex-col gap-2 basis-full" @submit.prevent="addMemo">
-        <p class="text-red-400" v-show="textAreaErrorHandler">memo tidak boleh kosong!</p>
-        <textarea v-model="formNewMemo" class="w-full basis-full rounded ps-2 pt-2"></textarea>
-        <button class="bg-slate-400 rounded px-2 py-1 w-fit self-end" type="submit">Submit</button>
+        <div class="flex gap-2">
+          <label for="title">Title</label>
+          <p class="text-red-400" v-show="formErrorHandler.title">| Title tidak boleh kosong!</p>
+        </div>
+        <input class="px-2 rounded p-2" placeholder="insert title here" id="title" v-model="inputMemo.title">
+        <div class="flex gap-2">
+          <label for="desc">Description</label>
+          <p class="text-red-400" v-show="formErrorHandler.content">| content tidak boleh kosong!</p>
+        </div>
+        <textarea v-model="inputMemo.content" class="w-full basis-full rounded px-2 pt-2" id="desc" placeholder="insert description here"></textarea>
+        <button class="bg-slate-400 rounded px-2 py-1 w-fit self-end hover:bg-[#121212]" type="submit">Submit</button>
       </form>
     </div>
   </div>
@@ -44,31 +52,57 @@
 
   interface memoItem {
     id: number,
+    title: string,
     content: string,
     date: string,
     backgroundColor: string
   }
 
+  interface formErrorHandler {
+    title: boolean,
+    content: boolean
+  }
+
+  interface formMemo {
+    title: string,
+    content: string
+  }
+
   const toggleModal = ref<boolean>(false)
-  const textAreaErrorHandler = ref<boolean>(false)
-  const formNewMemo = ref<string>('')
+  const formErrorHandler = ref<formErrorHandler>(
+    {
+      title : false,
+      content : false
+    }
+  )
+
+  const inputMemo = ref<formMemo>(
+    {
+      title: '', 
+      content: ''
+    }
+  )
+  
   const memos = ref< Array<memoItem> >([])
 
-  
-
   function addMemo() {
-    if(formNewMemo.value === '') {
-      textAreaErrorHandler.value = true
+    console.log(inputMemo.value.content);
+    if(inputMemo.value.title === '' || inputMemo.value.content === '') {
+      inputMemo.value.title === '' ? (formErrorHandler.value.title = true) : (formErrorHandler.value.title = false)
+      inputMemo.value.content === '' ? (formErrorHandler.value.content = true) : (formErrorHandler.value.content = false)
     }else{
       memos.value.push({
         id: Date.now(),
-        content: formNewMemo.value,
+        title: 'title',
+        content: inputMemo.value?.content,
         date: new Date().toLocaleDateString("en-GB"),
         backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(16)}`
       })
-      textAreaErrorHandler.value = false
       alert('Memo berhasil ditambahkan !')
-      formNewMemo.value = ''
+      inputMemo.value.content = ''
+      inputMemo.value.title = ''
+      formErrorHandler.value.content = false
+      formErrorHandler.value.title = false
       toggleModal.value = false
     }
   }
@@ -77,6 +111,14 @@
     const data = memos.value.filter(memo => memo.id === id)[0]
     alert("Memo pada id " + data.id + " berhasil dihapus")
     memos.value = memos.value.filter(memo => memo.id !== id)
+  }
+
+  function closeModal() {
+    toggleModal.value = false
+    inputMemo.value.title = ''
+    formErrorHandler.value.title = false
+    inputMemo.value.content = ''
+    formErrorHandler.value.content = false
   }
 </script>
 

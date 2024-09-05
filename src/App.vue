b732f66082c62ef841b9c30119deb0f6935ee371
Dewa Putra Hernanda <dewananda089@gmail.com>
2024-09-05 23:36:52 +0700

    create component for single memo

--- src/App.vue
+++ src/App.vue
@@ -3,38 +3,19 @@
     <h1 class="text-4xl font-bold">Memo app</h1>
 
     <div class="flex gap-3">
-      <!-- <input class="rounded-xl bg-slate-400 px-2" v-model="searchInput" /> -->
-      <modalForm />
+      <ModalForm />
     </div>
   </div>
 
   <div class="grid grid-cols-5 gap-4">
-    <div
-      v-for="memo in storeMemo.memos"
-      class="border-2 rounded-lg px-2 py-3 flex flex-col justify-between gap-2 h-[220px]"
-      :style="{ backgroundColor: memo.backgroundColor }"
-      :key="memo.id"
-    >
-      <h2 class="break-all line-clamp-1 text-black text-3xl font-bold">
-        {{ memo.title }}
-      </h2>
-      <p class="justify-evenly text-black line-clamp-4">{{ memo.content }}</p>
-      <div class="flex justify-between text-black">
-        <p class="border-t-2 border-black w-fit self-end">{{ memo.date }}</p>
-        <button
-          class="hover:text-white self-end"
-          @click="storeMemo.deleteMemo(memo.id)"
-        >
-          Delete
-        </button>
-      </div>
-    </div>
+    <Memo v-for="memo in storeMemo.memos" :key="memo.id" :memo="memo" />
   </div>
 </template>
 
 <script setup lang="ts">
   import { useMemosStore } from './utils/store'
-  import modalForm from './components/modalForm.vue'
+  import ModalForm from './components/modalForm.vue'
+  import Memo from './components/memo.vue'
 
   const storeMemo = useMemosStore()
 </script>
---
+++ src/components/Memo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="border-2 rounded-lg px-2 py-3 flex flex-col justify-between gap-2 h-[220px]"
+    :style="{ backgroundColor: memo.backgroundColor }"
+  >
+    <h2 class="break-all line-clamp-1 text-black text-3xl font-bold">
+      {{ memo.title }}
+    </h2>
+    <p class="justify-evenly text-black line-clamp-4">{{ memo.content }}</p>
+    <div class="flex justify-between text-black">
+      <p class="border-t-2 border-black w-fit self-end">{{ memo.date }}</p>
+      <button
+        class="hover:text-white self-end"
+        @click="storeMemo.deleteMemo(memo.id)"
+      >
+        Delete
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { defineProps } from 'vue'
+  import { memoItem } from '../types'
+  import { useMemosStore } from '../utils/store'
+
+  defineProps<{ memo: memoItem }>()
+  const storeMemo = useMemosStore()
+</script>
